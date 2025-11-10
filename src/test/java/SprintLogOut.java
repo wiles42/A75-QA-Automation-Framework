53a2fe29c758eb322940e6ffbb53e45ed9cb89ad
wiles42 <wiley.griffin@testpro.io>
2025-11-10 05:55:31 -0500

    LogOutTests
    LogOutTests

--- src/test/java/SprintLogOut.java
+++ src/test/java/SprintLogOut.java
@@ -19,13 +19,17 @@ public class SprintLogOut extends BaseTest {
     }
     @Test
     public void logInAndOut(){
+        //Precondition login
         LoginPage loginPage = new LoginPage(getDriver());
         HomePage homePage = new HomePage(getDriver());
         loginPage.provideEmail("wiley.griffin@testpro.io");
         loginPage.providePassword("oWRtHlx0");
         loginPage.clickSubmit();
+        // Verify Login button is present
         Assert.assertTrue(loginPage.logOutBtnVisible());
+        // Log Out
         loginPage.logout();
+       // Verify user is logged out 
         Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
     }
 
